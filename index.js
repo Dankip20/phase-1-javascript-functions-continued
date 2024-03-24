19f93cfb52e9e6e17277e28aaf4c8c7e871e2a6e
dan20 <kiptoodaniel92@gmail.com>
2024-03-24 15:30:23 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(act = 'roller-skate'){
+  let acti = `This Saturday, I want to ${act}!`
+  return acti
+}
+function mondayWork(act = 'go to the office'){
+  let acti = `This Monday, I will ${act}.`
+  return acti
+}
+
+function wrapAdjective(emph="*"){
+  return function (msg= "Special"){
+      return `You are ${emph}${msg}${emph}!`
+  }
+}
+
+
